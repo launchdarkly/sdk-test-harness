0ef80fbe383c7db0c89c82c43045e2f837215d07
Eli Bishop <eli@launchdarkly.com>
2022-03-08 18:01:33 -0800

    update parameterized tests with new test cases for U2C clause/target behavior

--- sdktests/server_side_eval.go
+++ sdktests/server_side_eval.go
@@ -24,55 +24,91 @@ func DoServerSideEvalTests(t *ldtest.T) {
 }
 
 func RunParameterizedServerSideEvalTests(t *ldtest.T) {
-	for _, suite := range getAllServerSideEvalTestSuites(t, "server-side-eval") {
-		t.Run(suite.Name, func(t *ldtest.T) {
-			if suite.RequireCapability != "" {
-				t.RequireCapability(suite.RequireCapability)
-			}
-
-			dataSource := NewSDKDataSource(t, suite.SDKData)
-			client := NewSDKClient(t, dataSource)
-
-			for _, test := range suite.Evaluations {
-				name := test.Name
-				if name == "" {
-					name = test.FlagKey
+	// We use the parameter expansion feature in the test data files in a few different ways, so for any
+	// given file we might end up with inputs that look like this--
+	//      test suite 1:
+	//			name: "things - test case 1"
+	//          sdkData: (data set 1)
+	//          evaluations: (name: "a")
+	//      test suite 2:
+	//			name: "things - test case 2"
+	//			sdkData: (data set 1)
+	//          evaluations: (name: "b)
+	//
+	// --or, both of those test suites might have the same name, "things". In the latter case, the test
+	// output looks nicer if we represent it as a parent test called "things" with subtests "a" and "b",
+	// rather than two parent tests called "things" with one subtest each. So, getAllServerSideEvalTests
+	// will group them together by the top-level name, and if we see more than one in a group then we
+	// know to go with the latter option.
+	for _, group := range getAllServerSideEvalTestSuites(t, "server-side-eval") {
+		if len(group) == 1 {
+			t.Run(group[0].Name, func(t *ldtest.T) {
+				runParameterizedTestSuite(t, group[0])
+			})
+		} else {
+			t.Run(group[0].Name, func(t *ldtest.T) {
+				for _, suite := range group {
+					runParameterizedTestSuite(t, suite)
 				}
-				t.Run(name, func(t *ldtest.T) {
-					t.Run("evaluate flag without detail", func(t *ldtest.T) {
-						params := makeEvalFlagParams(test, suite.SDKData)
-						result := client.EvaluateFlag(t, params)
-						m.In(t).Assert(result, EvalResponseValue().Should(m.Equal(test.Expect.Value)))
-					})
+			})
+		}
+	}
+}
 
-					t.Run("evaluate flag with detail", func(t *ldtest.T) {
-						params := makeEvalFlagParams(test, suite.SDKData)
-						params.Detail = true
-						result := client.EvaluateFlag(t, params)
-						m.In(t).Assert(result, m.AllOf(
-							EvalResponseValue().Should(m.Equal(test.Expect.Value)),
-							EvalResponseVariation().Should(m.Equal(test.Expect.VariationIndex)),
-							EvalResponseReason().Should(m.Equal(test.Expect.Reason)),
-						))
-					})
+func runParameterizedTestSuite(t *ldtest.T, suite testmodel.EvalTestSuite) {
+	if suite.RequireCapability != "" {
+		t.RequireCapability(suite.RequireCapability)
+	}
 
-					if !suite.SkipEvaluateAllFlags {
-						t.Run("evaluate all flags", func(t *ldtest.T) {
-							result := client.EvaluateAllFlags(t, servicedef.EvaluateAllFlagsParams{
-								Context: test.Context,
-							})
-							if test.Expect.VariationIndex.IsDefined() {
-								require.Contains(t, result.State, test.FlagKey)
-							}
-							expectedValue := test.Expect.Value
-							if !test.Expect.VariationIndex.IsDefined() {
-								expectedValue = ldvalue.Null()
-							}
-							m.In(t).Assert(result.State[test.FlagKey], m.Equal(expectedValue))
-						})
-					}
-				})
+	dataSource := NewSDKDataSource(t, suite.SDKData)
+	client := NewSDKClient(t, dataSource)
+
+	if len(suite.Evaluations) == 1 && suite.Evaluations[0].Name == "" {
+		runParameterizedTestEval(t, suite, suite.Evaluations[0], client)
+	} else {
+		for _, test := range suite.Evaluations {
+			name := test.Name
+			if name == "" {
+				name = test.FlagKey
 			}
+			t.Run(name, func(t *ldtest.T) {
+				runParameterizedTestEval(t, suite, test, client)
+			})
+		}
+	}
+}
+
+func runParameterizedTestEval(t *ldtest.T, suite testmodel.EvalTestSuite, test testmodel.EvalTest, client *SDKClient) {
+	t.Run("evaluate flag without detail", func(t *ldtest.T) {
+		params := makeEvalFlagParams(test, suite.SDKData)
+		result := client.EvaluateFlag(t, params)
+		m.In(t).Assert(result, EvalResponseValue().Should(m.Equal(test.Expect.Value)))
+	})
+
+	t.Run("evaluate flag with detail", func(t *ldtest.T) {
+		params := makeEvalFlagParams(test, suite.SDKData)
+		params.Detail = true
+		result := client.EvaluateFlag(t, params)
+		m.In(t).Assert(result, m.AllOf(
+			EvalResponseValue().Should(m.Equal(test.Expect.Value)),
+			EvalResponseVariation().Should(m.Equal(test.Expect.VariationIndex)),
+			EvalResponseReason().Should(m.Equal(test.Expect.Reason)),
+		))
+	})
+
+	if !suite.SkipEvaluateAllFlags {
+		t.Run("evaluate all flags", func(t *ldtest.T) {
+			result := client.EvaluateAllFlags(t, servicedef.EvaluateAllFlagsParams{
+				Context: test.Context,
+			})
+			if test.Expect.VariationIndex.IsDefined() {
+				require.Contains(t, result.State, test.FlagKey)
+			}
+			expectedValue := test.Expect.Value
+			if !test.Expect.VariationIndex.IsDefined() {
+				expectedValue = ldvalue.Null()
+			}
+			m.In(t).Assert(result.State[test.FlagKey], m.Equal(expectedValue))
 		})
 	}
 }
@@ -120,15 +156,27 @@ func RunParameterizedServerSideClientNotReadyEvalTests(t *ldtest.T) {
 	}
 }
 
-func getAllServerSideEvalTestSuites(t *ldtest.T, dirName string) []testmodel.EvalTestSuite {
+func getAllServerSideEvalTestSuites(t *ldtest.T, dirName string) [][]testmodel.EvalTestSuite {
+	// See comments in RunParameterizedServerSideEvalTests regarding the reason for grouping the
+	// results by name as we're doing here.
 	sources, err := testdata.LoadAllDataFiles(dirName)
 	require.NoError(t, err)
 
-	ret := make([]testmodel.EvalTestSuite, 0, len(sources))
+	ret := [][]testmodel.EvalTestSuite{}
+	var curName string
+	curGroup := []testmodel.EvalTestSuite{}
 	for _, source := range sources {
 		suite := parseServerSideEvalTestSuite(t, source)
-		ret = append(ret, suite)
+		if suite.Name != curName {
+			if curName != "" {
+				ret = append(ret, curGroup)
+				curGroup = []testmodel.EvalTestSuite{}
+			}
+			curName = suite.Name
+		}
+		curGroup = append(curGroup, suite)
 	}
+	ret = append(ret, curGroup)
 	return ret
 }
 
