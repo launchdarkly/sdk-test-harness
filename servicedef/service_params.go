72a474ae305877f6615397b7860830f8731801ea
Eli Bishop <eli@launchdarkly.com>
2022-04-12 20:10:49 -0700

    rm values not yet needed

--- servicedef/service_params.go
+++ servicedef/service_params.go
@@ -3,19 +3,15 @@ package servicedef
 import "github.com/launchdarkly/sdk-test-harness/framework/harness"
 
 const (
-	CapabilityClientSide    = "client-side"
 	CapabilityServerSide    = "server-side"
 	CapabilityStronglyTyped = "strongly-typed"
-	CapabilityMobile        = "mobile"
-	CapabilitySingleton     = "singleton"
 
 	CapabilityAllFlagsWithReasons                = "all-flags-with-reasons"
 	CapabilityAllFlagsClientSideOnly             = "all-flags-client-side-only"
 	CapabilityAllFlagsDetailsOnlyForTrackedFlags = "all-flags-details-only-for-tracked-flags"
 
-	CapabilityPersistentDataStore = "persistent-data-store"
-	CapabilityBigSegments         = "big-segments"
-	CapabilityTags                = "tags"
+	CapabilityBigSegments = "big-segments"
+	CapabilityTags        = "tags"
 )
 
 type StatusRep struct {
