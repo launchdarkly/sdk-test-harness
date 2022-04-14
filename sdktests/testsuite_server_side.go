509e5e5ede321695b7b4f498512fc831fbe4405f
Eli Bishop <eli@launchdarkly.com>
2022-04-14 10:24:58 -0700

    rm unused

--- sdktests/testsuite_server_side.go
+++ sdktests/testsuite_server_side.go
@@ -3,33 +3,11 @@ package sdktests
 import (
 	"time"
 
-	"github.com/launchdarkly/sdk-test-harness/framework/harness"
 	"github.com/launchdarkly/sdk-test-harness/framework/ldtest"
-	"github.com/launchdarkly/sdk-test-harness/mockld"
 	"github.com/launchdarkly/sdk-test-harness/servicedef"
 )
 
 const defaultEventTimeout = time.Second * 5
-
-func RunServerSideTestSuite(
-	harness *harness.TestHarness,
-	filter ldtest.Filter,
-	testLogger ldtest.TestLogger,
-) ldtest.Results {
-	config := ldtest.TestConfiguration{
-		Filter:       filter,
-		Capabilities: harness.TestServiceInfo().Capabilities,
-		TestLogger:   testLogger,
-		Context: SDKTestContext{
-			harness: harness,
-			sdkKind: mockld.ServerSideSDK,
-		},
-	}
-
-	return ldtest.Run(config, func(t *ldtest.T) {
-		doAllServerSideTests(t)
-	})
-}
 
 func doAllServerSideTests(t *ldtest.T) {
 	t.Run("data store", doServerSideDataStoreTests)
