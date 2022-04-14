de107be8f0ec906ed40c57b0be3b50a700526d43
Eli Bishop <eli@launchdarkly.com>
2022-04-13 20:43:41 -0700

    refactor for less extreme indentation

--- sdktests/client_side_eval.go
+++ sdktests/client_side_eval.go
@@ -29,92 +29,106 @@ func doClientSideEvalTests(t *ldtest.T) {
 }
 
 func runParameterizedClientSideEvalTests(t *ldtest.T) {
+	// For client-side SDKs, you have to tell the SDK at initialization time whether we'll be using evaluation
+	// reasons or not. The main effect of that is the client will add a withReasons query string parameter to
+	// its requests-- which we're not actually checking for here; we're just configuring the polling service
+	// to return the expected data based on an assumption of what the parameter is. But we're just being
+	// thorough in case for some reason there's some other unexpected difference in SDK behavior depending on
+	// the parameter.
 	allTestSuites := getAllClientSideEvalTestSuites(t, "client-side-eval")
 	for _, withReasons := range []bool{false, true} {
 		t.Run(fmt.Sprintf("evaluationReasons=%t", withReasons), func(t *ldtest.T) {
-			for _, suite := range allTestSuites {
-				t.Run(suite.Name, func(t *ldtest.T) {
-					if suite.RequireCapability != "" {
-						t.RequireCapability(suite.RequireCapability)
-					}
+			runParameterizedClientSideEvalTestsWithOrWithoutReasons(t, allTestSuites, withReasons)
+		})
+	}
+}
 
-					data := suite.SDKData
-					if !withReasons {
-						data = data.WithoutReasons()
-					}
+func runParameterizedClientSideEvalTestsWithOrWithoutReasons(
+	t *ldtest.T,
+	allTestSuites []testmodel.ClientSideEvalTestSuite,
+	withReasons bool,
+) {
+	for _, suite := range allTestSuites {
+		t.Run(suite.Name, func(t *ldtest.T) {
+			if suite.RequireCapability != "" {
+				t.RequireCapability(suite.RequireCapability)
+			}
 
-					// Here we configure the data source to return an initial data set. Mobile SDKs use streaming
-					// by default, and JS-based SDKs use polling by default; the simplest way to go is just to
-					// tell them explicitly to both use polling. We will cover streaming behavior in other tests.
-					var dataSource *SDKDataSource
-					filteredData := suite.SDKData
-					if !withReasons {
-						filteredData = filteredData.WithoutReasons()
-					}
-					var initialData mockld.SDKData = filteredData
-					if t.Capabilities().Has(servicedef.CapabilityMobile) {
-						initialData = mockld.MobileSDKData(filteredData) // this type makes the data source enable the mobile endpoints
-					}
-					dataSource = NewSDKDataSource(t, initialData, DataSourceOptionPolling)
-					client := NewSDKClient(
-						t,
-						WithClientSideConfig(servicedef.SDKConfigClientSideParams{
-							EvaluationReasons: o.Some(withReasons),
-							InitialUser:       suite.User,
-						}),
-						dataSource,
-					)
+			data := suite.SDKData
+			if !withReasons {
+				data = data.WithoutReasons()
+			}
 
-					for _, test := range suite.Evaluations {
-						name := test.Name
-						if name == "" {
-							name = test.FlagKey
-						}
-						t.Run(name, func(t *ldtest.T) {
-							t.Run("evaluate flag without detail", func(t *ldtest.T) {
-								params := makeClientSideEvalFlagParams(test, suite.SDKData)
-								result := client.EvaluateFlag(t, params)
-								m.In(t).Assert(result, EvalResponseValue().Should(m.Equal(test.Expect.Value)))
-							})
+			// Here we configure the data source to return an initial data set. Mobile SDKs use streaming
+			// by default, and JS-based SDKs use polling by default; the simplest way to go is just to
+			// tell them explicitly to both use polling. We will cover streaming behavior in other tests.
+			var dataSource *SDKDataSource
+			filteredData := suite.SDKData
+			if !withReasons {
+				filteredData = filteredData.WithoutReasons()
+			}
+			var initialData mockld.SDKData = filteredData
+			if t.Capabilities().Has(servicedef.CapabilityMobile) {
+				initialData = mockld.MobileSDKData(filteredData) // this type makes the data source enable the mobile endpoints
+			}
+			dataSource = NewSDKDataSource(t, initialData, DataSourceOptionPolling)
+			client := NewSDKClient(
+				t,
+				WithClientSideConfig(servicedef.SDKConfigClientSideParams{
+					EvaluationReasons: o.Some(withReasons),
+					InitialUser:       suite.User,
+				}),
+				dataSource,
+			)
 
-							t.Run("evaluate flag with detail", func(t *ldtest.T) {
-								params := makeClientSideEvalFlagParams(test, suite.SDKData)
-								params.Detail = true
-								result := client.EvaluateFlag(t, params)
+			for _, test := range suite.Evaluations {
+				name := test.Name
+				if name == "" {
+					name = test.FlagKey
+				}
+				t.Run(name, func(t *ldtest.T) {
+					t.Run("evaluate flag without detail", func(t *ldtest.T) {
+						params := makeClientSideEvalFlagParams(test, suite.SDKData)
+						result := client.EvaluateFlag(t, params)
+						m.In(t).Assert(result, EvalResponseValue().Should(m.Equal(test.Expect.Value)))
+					})
 
-								expectedReason := test.Expect.Reason
-								if !withReasons {
-									// If the client wasn't configured to request evaluation reasons, then there won't be
-									// any reasons that would have had to come from LD - but we can still get an error reason
-									// that is due to invalid SDK parameters.
-									switch expectedReason {
-									case ldreason.NewEvalReasonError(ldreason.EvalErrorFlagNotFound):
-										break
-									default:
-										expectedReason = ldreason.EvaluationReason{}
-									}
-								}
+					t.Run("evaluate flag with detail", func(t *ldtest.T) {
+						params := makeClientSideEvalFlagParams(test, suite.SDKData)
+						params.Detail = true
+						result := client.EvaluateFlag(t, params)
 
-								m.In(t).Assert(result, m.AllOf(
-									EvalResponseValue().Should(m.Equal(test.Expect.Value)),
-									EvalResponseVariation().Should(m.Equal(test.Expect.VariationIndex)),
-									EvalResponseReason().Should(EqualReason(expectedReason)),
-								))
-							})
-
-							if !suite.SkipEvaluateAllFlags {
-								t.Run("evaluate all flags", func(t *ldtest.T) {
-									result := client.EvaluateAllFlags(t, servicedef.EvaluateAllFlagsParams{})
-									if test.Expect.VariationIndex.IsDefined() {
-										require.Contains(t, result.State, test.FlagKey)
-									}
-									expectedValue := test.Expect.Value
-									if !test.Expect.VariationIndex.IsDefined() {
-										expectedValue = ldvalue.Null()
-									}
-									m.In(t).Assert(result.State[test.FlagKey], m.Equal(expectedValue))
-								})
+						expectedReason := test.Expect.Reason
+						if !withReasons {
+							// If the client wasn't configured to request evaluation reasons, then there won't be
+							// any reasons that would have had to come from LD - but we can still get an error reason
+							// that is due to invalid SDK parameters.
+							switch expectedReason {
+							case ldreason.NewEvalReasonError(ldreason.EvalErrorFlagNotFound):
+								break
+							default:
+								expectedReason = ldreason.EvaluationReason{}
 							}
+						}
+
+						m.In(t).Assert(result, m.AllOf(
+							EvalResponseValue().Should(m.Equal(test.Expect.Value)),
+							EvalResponseVariation().Should(m.Equal(test.Expect.VariationIndex)),
+							EvalResponseReason().Should(EqualReason(expectedReason)),
+						))
+					})
+
+					if !suite.SkipEvaluateAllFlags {
+						t.Run("evaluate all flags", func(t *ldtest.T) {
+							result := client.EvaluateAllFlags(t, servicedef.EvaluateAllFlagsParams{})
+							if test.Expect.VariationIndex.IsDefined() {
+								require.Contains(t, result.State, test.FlagKey)
+							}
+							expectedValue := test.Expect.Value
+							if !test.Expect.VariationIndex.IsDefined() {
+								expectedValue = ldvalue.Null()
+							}
+							m.In(t).Assert(result.State[test.FlagKey], m.Equal(expectedValue))
 						})
 					}
 				})
