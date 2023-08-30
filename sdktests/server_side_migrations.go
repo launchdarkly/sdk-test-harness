53911ff1ffe7cef019ba26a398e53451e14de6c4
Matthew Keeler <mkeeler@launchdarkly.com>
2023-08-30 14:38:48 -0400

    feat: Disable consistency through check ratio

--- sdktests/server_side_migrations.go
+++ sdktests/server_side_migrations.go
@@ -652,6 +652,11 @@ func successfulHandlersShouldNotGenerateErrorMetrics(t *ldtest.T) {
 }
 
 func trackConsistency(t *ldtest.T) {
+	t.Run("checks for correct stage", tracksConsistencyCorrectlyBasedOnStage)
+	t.Run("check ratio can disable", tracksConsistencyIsDisabledByCheckRatio)
+}
+
+func tracksConsistencyCorrectlyBasedOnStage(t *ldtest.T) {
 	handler := func(response string) func(w http.ResponseWriter, req *http.Request) {
 		return func(w http.ResponseWriter, req *http.Request) {
 			w.WriteHeader(http.StatusOK)
@@ -693,7 +698,7 @@ func trackConsistency(t *ldtest.T) {
 	}
 
 	for _, testParam := range testParams {
-		t.Run(fmt.Sprintf("%s errors for %s", testParam.Operation, testParam.Stage), func(t *ldtest.T) {
+		t.Run(fmt.Sprintf("%s consistency for %s", testParam.Operation, testParam.Stage), func(t *ldtest.T) {
 			client, events := createClient(t, stageToVariationIndex(testParam.Stage))
 
 			service := mockld.NewMigrationCallbackService(
@@ -761,14 +766,97 @@ func trackConsistency(t *ldtest.T) {
 	}
 }
 
+func tracksConsistencyIsDisabledByCheckRatio(t *ldtest.T) {
+	handler := func(w http.ResponseWriter, req *http.Request) { w.WriteHeader(http.StatusOK) }
+
+	testParams := []struct {
+		Operation ldmigration.Operation
+		Stage     ldmigration.Stage
+	}{
+		// Read operations
+		{Operation: ldmigration.Read, Stage: ldmigration.Off},
+		{Operation: ldmigration.Read, Stage: ldmigration.DualWrite},
+		{Operation: ldmigration.Read, Stage: ldmigration.Shadow},
+		{Operation: ldmigration.Read, Stage: ldmigration.Live},
+		{Operation: ldmigration.Read, Stage: ldmigration.RampDown},
+		{Operation: ldmigration.Read, Stage: ldmigration.Complete},
+
+		// Write operations -- we never run a consistency check for this
+		{Operation: ldmigration.Write, Stage: ldmigration.Off},
+		{Operation: ldmigration.Write, Stage: ldmigration.DualWrite},
+		{Operation: ldmigration.Write, Stage: ldmigration.Shadow},
+		{Operation: ldmigration.Write, Stage: ldmigration.Live},
+		{Operation: ldmigration.Write, Stage: ldmigration.RampDown},
+		{Operation: ldmigration.Write, Stage: ldmigration.Complete},
+	}
+
+	for _, testParam := range testParams {
+		t.Run(fmt.Sprintf("%s consistency for %s", testParam.Operation, testParam.Stage), func(t *ldtest.T) {
+			client, events := createClient(t, stageToVariationIndex(testParam.Stage))
+
+			service := mockld.NewMigrationCallbackService(requireContext(t).harness, t.DebugLogger(), handler, handler)
+			t.Defer(service.Close)
+
+			context := ldcontext.New("key")
+
+			params := servicedef.MigrationOperationParams{
+				Key:                "no-consistency-check",
+				Context:            context,
+				DefaultStage:       ldmigration.DualWrite,
+				ReadExecutionOrder: ldmigration.Concurrent,
+				OldEndpoint:        service.OldEndpoint().BaseURL(),
+				NewEndpoint:        service.NewEndpoint().BaseURL(),
+				Operation:          testParam.Operation,
+				TrackConsistency:   true,
+			}
+
+			_ = client.MigrationOperation(t, params)
+			client.FlushEvents(t)
+
+			opEventMatchers := []m.Matcher{
+				m.JSONOptProperty("samplingRatio").Should(m.BeNil()),
+				m.JSONProperty("operation").Should(m.Equal(string(testParam.Operation))),
+				m.JSONProperty("evaluation").Should(
+					m.AllOf(
+						m.JSONProperty("key").Should(m.Equal("no-consistency-check")),
+						m.JSONProperty("default").Should(m.Equal("dualwrite")),
+						m.JSONProperty("value").Should(m.Equal(string(testParam.Stage))),
+						m.JSONProperty("variation").Should(m.Equal(stageToVariationIndex(testParam.Stage))),
+						m.JSONProperty("reason").Should(
+							m.JSONProperty("kind").Should(m.Equal("FALLTHROUGH")),
+						),
+					),
+				),
+				m.JSONProperty("measurements").Should(m.Items(m.JSONProperty("key").Should(m.Equal("invoked")))),
+			}
+
+			payload := events.ExpectAnalyticsEvents(t, defaultEventTimeout)
+			m.In(t).Assert(payload, m.ItemsInAnyOrder(
+				IsIndexEventForContext(context),
+				IsSummaryEvent(),
+				IsValidMigrationOpEventWithConditions(
+					context,
+					opEventMatchers...,
+				),
+			))
+		})
+	}
+}
+
 func createClient(t *ldtest.T, variationIndex int) (*SDKClient, *SDKEventSink) {
 	migrationFlag := ldbuilders.NewFlagBuilder("migration-key").
 		On(true).
 		Variations(data.MakeStandardMigrationStages()...).
 		FallthroughVariation(variationIndex).
 		Build()
+	noConsistencyCheckFlag := ldbuilders.NewFlagBuilder("no-consistency-check").
+		On(true).
+		Variations(data.MakeStandardMigrationStages()...).
+		FallthroughVariation(variationIndex).
+		MigrationFlagParameters(ldbuilders.NewMigrationFlagParametersBuilder().CheckRatio(0).Build()).
+		Build()
 	dataBuilder := mockld.NewServerSDKDataBuilder()
-	dataBuilder.Flag(migrationFlag)
+	dataBuilder.Flag(migrationFlag, noConsistencyCheckFlag)
 
 	dataSource := NewSDKDataSource(t, dataBuilder.Build())
 	events := NewSDKEventSink(t)
