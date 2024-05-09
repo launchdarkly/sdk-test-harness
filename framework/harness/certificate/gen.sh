c9f134ac3c8bc5b1f74e7d0964946fe66801c33b
Casey Waldren <cwaldren@launchdarkly.com>
2024-05-09 11:36:23 -0700

    regen certificate: 10 years

--- framework/harness/certificate/gen.sh
+++ framework/harness/certificate/gen.sh
@@ -2,4 +2,8 @@
 
 set -e
 
-openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out certificates/cert.cert -keyout certificates/cert.key
+# Generates a certificate for testing purposes only. Validity 10 years.
+# Expires: May 7, 2034
+# If tests start failing because this expires, congratulations!
+
+openssl req -new -newkey rsa:4096 -x509 -sha256 -days 3650 -nodes -out cert.crt -keyout cert.key
