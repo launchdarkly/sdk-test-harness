ebbbea2e40bb1ce42681a1e2121ed9b56d125b70
Casey Waldren <cwaldren@launchdarkly.com>
2024-05-10 13:14:13 -0700

    fix tests

--- framework/harness/mock_endpoints_test.go
+++ framework/harness/mock_endpoints_test.go
@@ -15,7 +15,8 @@ import (
 )
 
 func TestMockEndpointServesRequest(t *testing.T) {
-	m := newMockEndpointsManager("testharness", 9998, 9999, framework.NullLogger())
+	m := newMockEndpointsManager("testharness",
+		map[string]int{"http": 9998, "https": 9999}, framework.NullLogger())
 
 	handler1 := httphelpers.HandlerWithStatus(200)
 	e1 := m.newMockEndpoint(handler1, framework.NullLogger())
@@ -23,43 +24,28 @@ func TestMockEndpointServesRequest(t *testing.T) {
 	handler2 := httphelpers.HandlerWithStatus(204)
 	e2 := m.newMockEndpoint(handler2, framework.NullLogger())
 
-	t.Run("http", func(t *testing.T) {
-		m.returnHTTPSEndpoints = false
+	for _, service := range []string{"http", "https"} {
+		t.Run(service, func(t *testing.T) {
+			m.SetService(service)
 
-		assert.Equal(t, "http://testharness:9998/endpoints/1", e1.BaseURL())
-		assert.Equal(t, "http://testharness:9998/endpoints/2", e2.BaseURL())
+			assert.Equal(t, service+"://testharness:9998/endpoints/1", e1.BaseURL())
+			assert.Equal(t, service+"://testharness:9998/endpoints/2", e2.BaseURL())
 
-		rr1 := httptest.NewRecorder()
-		r1, _ := http.NewRequest("GET", e1.BaseURL(), nil)
-		m.serveHTTP(rr1, r1)
-		assert.Equal(t, 200, rr1.Code)
+			rr1 := httptest.NewRecorder()
+			r1, _ := http.NewRequest("GET", e1.BaseURL(), nil)
+			m.serveHTTP(rr1, r1)
+			assert.Equal(t, 200, rr1.Code)
 
-		rr2 := httptest.NewRecorder()
-		r2, _ := http.NewRequest("GET", e2.BaseURL(), nil)
-		m.serveHTTP(rr2, r2)
-		assert.Equal(t, 204, rr2.Code)
-	})
-
-	t.Run("returnHTTPSEndpoints", func(t *testing.T) {
-		m.returnHTTPSEndpoints = true
-
-		assert.Equal(t, "https://testharness:9999/endpoints/1", e1.BaseURL())
-		assert.Equal(t, "https://testharness:9999/endpoints/2", e2.BaseURL())
-
-		rr1 := httptest.NewRecorder()
-		r1, _ := http.NewRequest("GET", e1.BaseURL(), nil)
-		m.serveHTTP(rr1, r1)
-		assert.Equal(t, 200, rr1.Code)
-
-		rr2 := httptest.NewRecorder()
-		r2, _ := http.NewRequest("GET", e2.BaseURL(), nil)
-		m.serveHTTP(rr2, r2)
-		assert.Equal(t, 204, rr2.Code)
-	})
+			rr2 := httptest.NewRecorder()
+			r2, _ := http.NewRequest("GET", e2.BaseURL(), nil)
+			m.serveHTTP(rr2, r2)
+			assert.Equal(t, 204, rr2.Code)
+		})
+	}
 }
 
 func TestMockEndpointReceivesSubpath(t *testing.T) {
-	m := newMockEndpointsManager("testharness", 9998, 9999, framework.NullLogger())
+	m := newMockEndpointsManager("testharness", map[string]int{"http": 9998}, framework.NullLogger())
 
 	handler, requests := httphelpers.RecordingHandler(httphelpers.HandlerWithStatus(200))
 	e := m.newMockEndpoint(handler, framework.NullLogger())
@@ -79,7 +65,7 @@ func TestMockEndpointReceivesSubpath(t *testing.T) {
 }
 
 func TestMockEndpointConnectionInfo(t *testing.T) {
-	m := newMockEndpointsManager("testharness", 9998, 9999, framework.NullLogger())
+	m := newMockEndpointsManager("testharness", map[string]int{"http": 9998}, framework.NullLogger())
 	handler := httphelpers.HandlerWithStatus(200)
 	e := m.newMockEndpoint(handler, framework.NullLogger())
 
