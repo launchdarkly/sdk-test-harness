bd093c9e1fc53f574da9feefd3de7cd32ce201af
Matthew Keeler <mkeeler@launchdarkly.com>
2025-03-13 15:37:27 -0400

    fix: Replace custom event redaction test with appropriate capability

--- sdktests/common_tests_events_custom.go
+++ sdktests/common_tests_events_custom.go
@@ -73,7 +73,7 @@ func (c CommonEventTests) CustomEvents(t *ldtest.T) {
 		}
 	})
 
-	if t.Capabilities().Has(servicedef.CapabilityAnonymousRedaction) {
+	if t.Capabilities().Has(servicedef.CapabilityOmitAnonymousContexts) {
 		t.Run("single-kind anonymous context redacts all attributes", func(t *ldtest.T) {
 			anonymousFactory := data.NewContextFactory("anonymous", func(b *ldcontext.Builder) {
 				b.Anonymous(true)
