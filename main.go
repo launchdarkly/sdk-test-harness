de833af990da23a89b66c5366809b5be8c27e3f8
Jason Bailey <jbailey@launchdarkly.com>
2026-01-28 09:54:20 -0600

    chore: Bumping to 3.0.0-alpha.2 (#312)

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 )
 
 const defaultPort = 8111
-const versionString string = "3.0.0-alpha.1" // {{ x-release-please-version }}
+const versionString string = "3.0.0-alpha.2" // {{ x-release-please-version }}
 
 func main() {
 	fmt.Printf("sdk-test-harness v%s\n", strings.TrimSpace(versionString))
