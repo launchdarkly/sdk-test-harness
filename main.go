cfbb12f2d2301fb9096c0f8d924897e3a45e5f2f
Aaron Zeisler <azeisler@launchdarkly.com>
2026-03-05 13:15:47 -0800

    chore: Update version tag to alpha.4

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 )
 
 const defaultPort = 8111
-const versionString string = "3.0.0-alpha.3" // {{ x-release-please-version }}
+const versionString string = "3.0.0-alpha.4" // {{ x-release-please-version }}
 
 func main() {
 	fmt.Printf("sdk-test-harness v%s\n", strings.TrimSpace(versionString))
