2a57f075cf9cc0ded232bf6694a7184b267dbb79
Steven Zhang <szhang@launchdarkly.com>
2026-09-21 14:34:38 -0400

    feat: adding client side bootstrap tests

--- servicedef/service_params.go
+++ servicedef/service_params.go
@@ -18,7 +18,13 @@ const (
 	CapabilityAllFlagsClientSideOnly             = "all-flags-client-side-only"
 	CapabilityAllFlagsDetailsOnlyForTrackedFlags = "all-flags-details-only-for-tracked-flags"
 
-	CapabilityBigSegments                 = "big-segments"
+	CapabilityBigSegments = "big-segments"
+
+	// CapabilityBootstrap indicates a client-side SDK can be configured with pre-fetched flag
+	// data ("bootstrap" data) that it applies to its flag store at start time, before making
+	// any network requests, and that alone satisfies the SDK's initialization condition.
+	CapabilityBootstrap = "bootstrap"
+
 	CapabilityContextType                 = "context-type"
 	CapabilityContextComparison           = "context-comparison"
 	CapabilitySecureModeHash              = "secure-mode-hash"
